f504bc77d4fbf279503c1c83afba86389cf26228
Krzysztof Kłos <klos.krzysztof94@gmail.com>
2020-08-17 21:32:30 +0200

    Hydraap v 1.0.0

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,34 @@ registerSW();
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+const counter = document.querySelector('.hydrapp__counter--js');
+const addButton = document.querySelector('.hydraap__add-button--js');
+const removeButton = document.querySelector('.hydrapap__remove-button--js');
+
+const minimumGlassNumber = 0;
+let currentGlassCounter = 0;
+
+const key = new Date().toISOString().slice(0, 10);
+const localStorageValue = localStorage.getItem(key);
+
+if (localStorageValue) {
+    currentGlassCounter = localStorageValue;
+} else {
+    localStorage.setItem(key, 0);
+}
+
+counter.innerHTML = currentGlassCounter;
+
+addButton.addEventListener('click', e => {
+    currentGlassCounter++;
+    counter.innerHTML = currentGlassCounter;
+    localStorage.setItem(key, currentGlassCounter);
+})
+
+removeButton.addEventListener('click', e => {
+    if (currentGlassCounter > minimumGlassNumber) {
+        currentGlassCounter--;
+    }
+    counter.innerHTML = currentGlassCounter;
+    localStorage.setItem(key, currentGlassCounter);
+})
